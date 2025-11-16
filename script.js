7a9296f0657ad0d7ef5264bb86bc18326324c62c
sindhukatta794 <Sindhukatta9@gmail.com>
2025-11-15 18:36:26 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Smooth scrolling
+document.querySelectorAll('nav a').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    document.querySelector(link.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
+  });
+});
